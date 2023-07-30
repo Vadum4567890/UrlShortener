c310b437c6bd84811b26907597e0837926d97c17
vadimyff <solpo789@gmail.com>
2023-07-30 03:54:26 +0300

    Add some info

--- UrlShortener/Data/ApplicationDbContext.cs
+++ UrlShortener/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 using UrlShortener.Models;
 
 namespace UrlShortener.Data
@@ -9,11 +10,12 @@ namespace UrlShortener.Data
     {
         public DbSet<Url> Urls { get; set; }
         public DbSet<AboutModel> AboutModel { get; set; }
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IHttpContextAccessor httpContextAccessor)
             : base(options)
         {
-            
+            _httpContextAccessor = httpContextAccessor;
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -45,6 +47,19 @@ namespace UrlShortener.Data
                 UserId = adminUser.Id,
                 RoleId = adminRoleId
             });
+        }
+
+        public IdentityUser GetCurrentUser()
+        {
+            var userId = _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (userId != null)
+            {
+                // Знайдено ідентифікатор користувача, можна виконати запит до бази даних, щоб отримати потрібного користувача
+                return this.Users.FirstOrDefault(u => u.Id == userId);
+            }
+
+            // Якщо ідентифікатор користувача не знайдено або користувач не авторизований, поверніть null або зробіть інше відповідне дії
+            return null;
         }
     }
 }
---
+++ UrlShortner/Data/DataSeeder.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortner.Models;
+
+namespace UrlShortner.Data
+{
+    public class DataSeeder
+    {
+        public static void SeedData(ApplicationDbContext dbContext)
+        {
+            // Check if the database is empty or needs seeding
+            if (!dbContext.AlgorithmDescriptions.Any())
+            {
+                // Add initial data here
+                var algorithmDescription = new Models.AlgorithmDescription
+                {
+                    Description = "Default Description"
+                };
+
+                dbContext.AlgorithmDescriptions.Add(algorithmDescription);
+                dbContext.SaveChanges();
+            }
+
+            if (!dbContext.Urls.Any())
+            {
+                var url1 = new Url { CreatedBy = "meuf", CreatedDate = DateTime.Now, ShortUrl = "https://google.com", OriginalUrl = "https://google.com/maps" };
+                var url2 = new Url { CreatedBy = "keuf", CreatedDate = DateTime.Now, ShortUrl = "https://youtube.com", OriginalUrl = "https://youtube.com/fdsdssn31341nsl" };
+                dbContext.Urls.AddRange(url1,url2);
+                dbContext.SaveChanges();
+            }
+        }
+    }
+}
